7120d2dd0cf26efeae36b2a4bbd937613d1d2d1e
Darío Mejía <72144279+DarioMejia@users.noreply.github.com>
2023-09-13 11:53:02 -0500

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:mathlingo/domain/use_case/user_usecase.dart';
+import 'package:mathlingo/controller/authentication_controller.dart';
+import 'package:mathlingo/controller/user_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mathlingo/pages/HomePage.dart';
 
+import 'domain/repositories/repository.dart';
+import 'domain/use_case/authentication_usecase.dart';
+
 void main() {
-  // Get.put(controller());
+  Get.put(Repository());
+  Get.put(AuthenticationUseCase());
+  Get.put(UserUseCase());
+  Get.put(AuthenticationController());
+  Get.put(UserController());
 
   runApp(const MyApp());
 }
